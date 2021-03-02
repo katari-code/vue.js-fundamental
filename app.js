0d73dd6d8bcc8648eee5ac48ba2e8762a56b8202
Ibrahim Katari <elsaid@live.utm.my>
2021-03-02 10:20:48 +0200

    using CDN

---
+++ app.js
@@ -0,0 +1,14 @@
+
+const app = Vue.createApp({
+    // data, functions 
+    data(){
+        return{
+            title: "The Final Oprrutintes",
+            author: "author",
+            age : 45
+        }
+    },
+})
+
+
+app.mount('#app')
